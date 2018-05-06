ba9655adcc9e8f00c02835470175303b3eb4be6d
mccorn <mccorn13@gmail.com>
2018-05-07 00:42:01 +0300

    v2.1.01

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var timerSmall = 2500;
 // Model:Состояние
 let model = {
     FULLCARDSSET : ["2C","2D","2H","2S","3C","3D","3H","3S","4C","4D","4H","4S","5C","5D","5H","5S","6C","6D","6H","6S","7C","7D","7H","7S","8C","8D","8H","8S","9C","9D","9H","9S","0C","0D","0H","0S","AC","AD","AH","AS","JC","JD","JH","JS","KC","KD","KH","KS","QC","QD","QH","QS"],
-    PAIRSCOUNT : 3,
+    PAIRSCOUNT : 6,
 
     cardsset : [],
     score : 0,
@@ -49,44 +49,43 @@ var controller = {
         model.init();
         viewer.init();
     },
-    
-    //$('.openedcard').each(function (){ if ($(this).attr("data-tm")) { console.log($(this).attr("data-tm"))}})
+
     gameAction: function(somecard){
         model.activedcards++;
         if ($('.openedcard').length <= 1) {
             model.addlog($(somecard).data('tm'));
+//            console.log($(somecard).data('tm'));
             viewer.flipCard($(somecard));
-            
-        }
-        if (!($('.openedcard').length % 2)) {
-            if (model.history[model.history.length - 1] != model.history[model.history.length - 2]) 
-            {
-                $('.card').prop("disabled", true);
-                setTimeout( function(){
-                    viewer.flipCard($(".openedcard"));
-                    model.updateModel(false);
-                    viewer.getScore();
-                },timerSmall);           
-            }
-            else {
-                $('.card').prop("disabled", true);
-                setTimeout( function(){
-                    $(".openedcard").empty();
-                    $(".openedcard").removeClass('openedcard');
-                    model.updateModel(true);
-                    viewer.getScore();
-                },timerSmall);  
-                if ( $('.closedcard').length == 2) {
-                    $('.restartsubmit').prop("disabled", true);
-                    model.updateModel(true);
-                    viewer.openCardsSet();
-                    viewer.getScore(); 
-                    setTimeout( function(){ 
-                        viewer.showScreenByClass('.finishscreen');
-                    },timerSmall);          
+            if ($('.openedcard').length === 2) {
+                if (model.history[model.history.length - 1] != model.history[model.history.length - 2]) 
+                {
+//                    $('.card').prop("disabled", true);
+                    setTimeout( function(){
+                        viewer.flipCard($(".openedcard"));
+                        model.updateModel(false);
+                        viewer.getScore();
+                    },timerSmall);           
                 }
-            }
-        } 
+                else {
+//                    $('.card').prop("disabled", true);
+                    setTimeout( function(){
+                        $(".openedcard").empty();
+                        $(".openedcard").removeClass('openedcard');
+                        model.updateModel(true);
+                        viewer.getScore();
+                    },timerSmall);  
+                    if ( $('.closedcard').length == 2) {
+                        $('.restartsubmit').prop("disabled", true);
+                        model.updateModel(true);
+                        viewer.openCardsSet();
+                        viewer.getScore(); 
+                        setTimeout( function(){ 
+                            viewer.showScreenByClass('.finishscreen');
+                        },timerSmall);          
+                    }
+                }
+            } 
+        }
         setTimeout( function(){
             $('.card').prop("disabled", false);
         },timerSmall);  
